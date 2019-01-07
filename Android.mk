106fa8e5ca3d99a943dfe34c59e415b47ff16416
Marko Man <darkobas@gmail.com>
2019-01-07 13:51:05 +0100

    adjust for omni

--- Android.mk
+++ Android.mk
@@ -1,17 +1,24 @@
+ifeq ($(TARGET_QCOM_DISPLAY_VARIANT),caf-sdm660)
+
 sdm-libs := sdm/libs
 display-hals := include $(sdm-libs)/utils $(sdm-libs)/core
 
 ifneq ($(TARGET_IS_HEADLESS), true)
-    display-hals += libcopybit liblight libmemtrack hdmi_cec \
+    display-hals += libcopybit libmemtrack hdmi_cec \
                     $(sdm-libs)/hwc $(sdm-libs)/hwc2 gpu_tonemapper libdrmutils libdisplayconfig
 endif
 
 display-hals += gralloc
+
+ifneq ($(TARGET_PROVIDES_LIBLIGHT),true)
+display-hals += liblight
+endif
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
     include $(call all-named-subdir-makefiles,$(display-hals))
 else
 ifneq ($(filter msm% apq%,$(TARGET_BOARD_PLATFORM)),)
     include $(call all-named-subdir-makefiles,$(display-hals))
+endif
 endif
 endif
